0f06864a085759ebe73c07aa2f08a97483431392
TinzarTun <tinzartun2002@gmail.com>
2026-02-04 23:42:16 +0630

    cookie-1.php, cookie-2.php

---
+++ cookie-1.php
@@ -0,0 +1,22 @@
+<?php 
+    if (isset($_POST['btnsend'])) 
+    {
+        $name=$_POST['txtname'];
+        setcookie("username",$name,time()+60);
+    }
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+    <title></title>
+ </head>
+ <body>
+    <form action="cookie1.php" method="post">
+        Name: <input type="text" name="txtname">
+        <br>
+        <input type="submit" name="btnsend" value="Send">
+    </form>
+    <a href="cookie-2.php">Go to Cookie2 Page</a>
+ </body>
+ </html>
