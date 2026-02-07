db80ceee2a8685adc064b3e28e44704a93621c31
TinzarTun <tinzartun2002@gmail.com>
2026-02-07 20:58:33 +0630

    dice roll

---
+++ Advance/diceForm.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dice Form</title>
+</head>
+<body>
+    <form action="diceRoll.php" method="get">
+        <p>How many dice?</p>
+        <input type = "text" name = "num">
+        <P>How many faces?</P>
+        <input type = "text" name = "faces">
+        <input type = "submit" value = "Roll">
+        <input type = "reset" value = "Clear values">
+    </form>
+</body>
+</html>
